97a87776d028facaace04582afeb181aa6746c29
Pedro Duarte <pduartesilva2005@gmail.com>
2025-02-20 20:00:01 -0300

    Create the invite page & separate in the components

---
+++ src/app/invite/invite-link-input.tsx
@@ -0,0 +1,22 @@
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { Copy, Link } from 'lucide-react'
+
+export function InviteLinkInput() {
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link className="size-5" />
+      </InputIcon>
+
+      <InputField
+        readOnly
+        defaultValue="http://localhost:3000/invite/13837837837"
+      />
+
+      <IconButton className="-mr-2">
+        <Copy className="size-5" />
+      </IconButton>
+    </InputRoot>
+  )
+}
