564d5b08bb948b9f8a83ffc8efa74e461a28b3e0
Pedro Duarte <pduartesilva2005@gmail.com>
2025-02-19 22:29:34 -0300

    Starting the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      <h1 className="text-4xl font-semibold font-heading">Hello World</h1>
     </main>
   );
 }
